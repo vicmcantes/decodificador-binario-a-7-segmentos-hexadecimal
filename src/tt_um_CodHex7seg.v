d80a40f9b441b488f3d3645632120415a45b1554
vicmcantes <165434004+vicmcantes@users.noreply.github.com>
2024-03-29 22:11:53 -0600

    Update and rename project.v to tt_um_CodHex7seg.v

--- src/tt_um_CodHex7seg.v
+++ src/tt_um_CodHex7seg.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_example (
+module tt_um_CodHex7seg (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,8 +17,9 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    CodHex7seg U1 (.h(ui_in[3:0]), .S(uo_out[6:0]));
+    assign uio_oe = 8'00000000;
+    assign uio_out = 8'b00000000;
+    assign uo_out[7] = 1'b0;
 
 endmodule
